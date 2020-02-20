d8f89c9dbfe2688965f8e7d47e15f1ee54926e48
Stanley Ulili <stanleyulili@gmail.com>
2020-02-20 10:51:30 +0200

    (feat): show 404 errors for all urls path not defined in the app

--- app.js
+++ app.js
@@ -5,15 +5,22 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  fs.readFile('./templates/index.html', (error, data) => {
-    if (error) {
-      throw error;
-    }
+  if (req.url === '/') {
+    fs.readFile('./templates/index.html', (error, html) => {
+      if (error) throw error;
 
-    res.writeHead('200', { 'Content-Type': 'text/html' });
-    res.write(data);
-    res.end();
-  });
+      res.writeHead('200', { 'Content-Type': 'text/html' });
+      res.write(html);
+      res.end();
+    });
+  } else {
+    fs.readFile('./templates/404.html', (error, html) => {
+      if (error) throw error;
+      res.writeHead('200', { 'Content-Type': 'text/html' });
+      res.write(html);
+      res.end();
+    });
+  }
 });
 
 server.listen(port, hostname, () => {
