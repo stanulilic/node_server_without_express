6e5e2d9266973f32d0f5820d9d2553b0d6acdbb4
Stanley Ulili <stanleyulili@gmail.com>
2020-02-20 10:27:08 +0200

    (feat): show index page from html file in the browser

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('hello world');
+const http = require('http');
+const fs = require('fs');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  fs.readFile('./templates/index.html', (error, data) => {
+    if (error) {
+      throw error;
+    }
+
+    res.writeHead('200', { 'Content-Type': 'text/html' });
+    res.write(data);
+    res.end();
+  });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
+});
